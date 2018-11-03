e94c219d80500c556fa120fd825b14703446db32
Davide Vacatello <davide.vacatello@gmail.com>
2018-11-03 17:00:01 +0100

    edit

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const {MongoClient, ObjectID} = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
     if (err) {
@@ -18,30 +18,40 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
             return console.log('Unable to insert todo', err)
         }
 
+        console.log('Create');
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
     */
 
     // READ
     db.collection('Todos').find().toArray().then((result) => {
-        console.log('Todos');
+        console.log('Read');
         console.log(JSON.stringify(result, undefined, 2));
     }).catch((e) => {
         console.log(e);
     });
 
     db.collection('Todos').find({completed: false}).toArray().then((result) => {
-        console.log('Todos');
+        console.log('Read2');
         console.log(JSON.stringify(result, undefined, 2));
     }).catch((e) => {
         console.log(e);
     });
 
     // UPDATE
+    db.collection('Todos').findOneAndUpdate({completed: false}, {$set:{completed: true}}, {returnOriginal: false})
+        .then((result) => {
+            console.log('Update');
+            console.log(result);
+        });
 
-
+    /*
     // DELETE
-
+    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+        console.log('Delete');
+        console.log(result);
+    });
+    */
 
     client.close();
 });
